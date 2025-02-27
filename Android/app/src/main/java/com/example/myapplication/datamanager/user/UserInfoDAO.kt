de04cfa6681503dbe3b3ab1215a8067b297c1916
jasmine <jasmine.yabari02@gmail.com>
2025-02-27 17:49:09 +0200

    Create User

---
+++ Android/app/src/main/java/com/example/myapplication/datamanager/user/UserInfoDAO.kt
@@ -0,0 +1,41 @@
+package com.example.myapplication.datamanager.user
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface UserInfoDAO {
+    @Query("SELECT * FROM UserInfo")
+    fun getAll(): List<UserInfo>
+
+    @Query("SELECT * FROM UserInfo WHERE user_id LIKE :userId LIMIT 1")
+    fun findByUserId(userId: Int): UserInfo
+
+    @Query("SELECT * FROM UserInfo WHERE user_id IN " +
+            "(SELECT uid, username FROM User WHERE username = :username) LIMIT 1")
+    fun findByUsername(username: String): UserInfo
+
+    @Query("UPDATE UserInfo SET height = :height WHERE user_id IN " +
+            "(SELECT uid, username FROM User WHERE username = :username)")
+    fun updateUserHeightByUsername(username: String, height: Int)
+
+    @Query("UPDATE UserInfo SET height = :weight WHERE user_id IN " +
+            "(SELECT uid, username FROM User WHERE username = :username)")
+    fun updateUserWeightByUsername(username: String, weight: Float)
+
+    @Query("DELETE FROM UserInfo WHERE user_id IN " +
+            "(SELECT uid, username FROM User WHERE username = :username)")
+    fun deleteUserInfoByUsername(username: String)
+
+    @Update
+    fun update(userInfo: UserInfo)
+
+    @Insert
+    fun insert(userInfo: UserInfo)
+
+    @Delete
+    fun delete(userInfo: UserInfo)
+}
