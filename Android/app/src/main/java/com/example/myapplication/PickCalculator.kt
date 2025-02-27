d4cb594330a41c45a883f5137254a0ce5bd860f1
KStanev03 <kristianstanevv03@gmail.com>
2025-02-28 00:51:20 +0200

    Pick-Calculator-View

---
+++ Android/app/src/main/java/com/example/myapplication/PickCalculator.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.Button
+
+
+class PickCalculator : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pick_calculator)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnCalculatorBmi = findViewById<Button>(R.id.btnCalculatorBmi)
+        btnCalculatorBmi.setOnClickListener {
+            val intent= Intent(this,Calculator::class.java)
+            startActivity(intent)
+        }
+    }
+}
