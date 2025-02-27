c9515dba05c5cb97f03986fbafc7f04e42d5e265
jasmine <jasmine.yabari02@gmail.com>
2025-02-27 18:12:01 +0200

    Changes

--- Android/app/src/main/java/com/example/myapplication/dailydata/DailyDataDAO.kt
+++ Android/app/src/main/java/com/example/myapplication/dailydata/DailyDataDAO.kt
@@ -4,7 +4,6 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
-import com.example.myapplication.datamanager.user.UserInfo
 import java.sql.Date
 
 @Dao
--- Android/app/src/main/java/com/example/myapplication/datamanager/activity/ActivityDAO.kt
+++ Android/app/src/main/java/com/example/myapplication/datamanager/activity/ActivityDAO.kt
@@ -5,7 +5,6 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
-import com.example.myapplication.datamanager.user.NutritionInfo
 
 @Dao
 interface ActivityDAO {
