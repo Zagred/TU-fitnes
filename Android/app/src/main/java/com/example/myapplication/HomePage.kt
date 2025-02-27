a58d3f5863d200ee07dca16748b0ad618f040fa0
plamen <looopooo45@gmail.com>
2025-02-27 12:57:30 +0200

    Added calendar page, bmi calculator they are working profile and woorkout page are jsut added nothing here (need to be made another calculator for calories page and to select dates in the calendar) plus xml file where id is button 1 to 9 need to be change with normal id's

--- Android/app/src/main/java/com/example/myapplication/HomePage.kt
+++ Android/app/src/main/java/com/example/myapplication/HomePage.kt
@@ -1,20 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home_page)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val calculaterl=findViewById<Button>(R.id.button9)
+        val calendar=findViewById<Button>(R.id.button3)
+
+        calculaterl.setOnClickListener{
+            val intent= Intent(this,Calculator::class.java)
+            startActivity(intent)
+        }
+        calendar.setOnClickListener {
+            val intent=Intent(this,Calendar::class.java)
+            startActivity(intent)
         }
     }
 }
--- Android/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Android/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +12,16 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val userName=findViewById<EditText>(R.id.edUsername)
+        val password=findViewById<EditText>(R.id.edPassword)
+        val login=findViewById<Button>(R.id.btLogin)
+        val register=findViewById<Button>(R.id.btRegister)
+
+        //smenyane mejdu stranicite
+        login.setOnClickListener{
+            val intent=Intent(this,HomePage::class.java)
+            startActivity(intent)
         }
     }
 }
