b947b95a3ffa950d935460164f518f80bffbbcb4
jasmine <jasmine.yabari02@gmail.com>
2025-03-21 11:26:43 +0200

    add Class model

--- CalorieCounterTU/CalorieCounter/fitness/models.py
+++ CalorieCounterTU/CalorieCounter/fitness/models.py
@@ -4,7 +4,7 @@ from django.core import validators
 from django.db import models
 
 from CalorieCounter.accounts.validators import validate_age_through_birthday
-from CalorieCounter.fitness.validators import validate_name
+from CalorieCounter.fitness.validators import validate_name, validate_date
 
 
 class Trainer(models.Model):
@@ -72,8 +72,61 @@ class Trainer(models.Model):
             age -= 1
         return age
 
-    def __str__(self):
+    @property
+    def full_name(self):
         return f'{self.first_name} {self.last_name}'
 
     class Meta:
         ordering = ('first_name', 'last_name',)
+
+
+class Class(models.Model):
+    MAX_NAME_LEN = 50
+    MIN_DURATION_IN_MINUTES = 10
+    MAX_DESCRIPTION_LEN = 250
+    MIN_USER_CAPACITY = 5
+
+    DURATION_IN_MIN_ERROR_MSG = f'Duration must be at least {MIN_DURATION_IN_MINUTES} seconds.'
+    INVALID_URL_MSG = "Invalid URL!"
+
+    name = models.CharField(
+        max_length=MAX_NAME_LEN,
+    )
+
+    duration_in_minutes = models.IntegerField(
+        validators=[validators.MinValueValidator(MIN_DURATION_IN_MINUTES),],
+        error_messages={
+            'min_value': DURATION_IN_MIN_ERROR_MSG
+        }
+    )
+
+    time = models.TimeField()
+
+    date = models.DateField(
+        validators=[validate_date]
+    )
+
+    description = models.TextField(
+        max_length=MAX_DESCRIPTION_LEN,
+    )
+
+    max_user_capacity = models.IntegerField(
+        validators=[validators.MinValueValidator(MIN_USER_CAPACITY)]
+    )
+
+    image_url = models.URLField(
+        validators=[validators.URLValidator()],
+        error_messages={
+            'url': INVALID_URL_MSG,
+        }
+    )
+
+    trainer_id = models.ForeignKey(
+        to=Trainer,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        ordering = ('name',)
