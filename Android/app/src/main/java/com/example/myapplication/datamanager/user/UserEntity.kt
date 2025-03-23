4f62e40b9adfb42a9bed24882aab91ca9226a014
KStanev03 <kristianstanevv03@gmail.com>
2025-03-23 16:37:00 +0200

    adding-role

--- Android/app/src/main/java/com/example/myapplication/datamanager/user/UserEntity.kt
+++ Android/app/src/main/java/com/example/myapplication/datamanager/user/UserEntity.kt
@@ -5,12 +5,12 @@ import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
 
-
 @Entity(tableName = "User", indices = [Index(value = ["username"], unique = true)])
 data class User(
     @PrimaryKey(autoGenerate = true) val uid: Int = 0,
     @ColumnInfo(name = "username") val username: String,
     @ColumnInfo(name = "password") val password: String,
+    @ColumnInfo(name = "role", defaultValue = "user") val role: String = "user" // Default role is "user"
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -21,6 +21,7 @@ data class User(
         if (uid != other.uid) return false
         if (username != other.username) return false
         if (password != other.password) return false
+        if (role != other.role) return false
 
         return true
     }
@@ -29,6 +30,7 @@ data class User(
         var result = uid
         result = 31 * result + username.hashCode()
         result = 31 * result + password.hashCode()
+        result = 31 * result + role.hashCode()
         return result
     }
 }
