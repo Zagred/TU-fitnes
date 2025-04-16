e7e0a5c4ce332a84a1494afdc1a242f4de5cddce
teodoradmt <115478970+teodoradmt@users.noreply.github.com>
2025-04-17 00:10:37 +0300

    forgot 2 avatar pics commit

--- Android/app/src/main/java/com/example/myapplication/Profile.kt
+++ Android/app/src/main/java/com/example/myapplication/Profile.kt
@@ -92,7 +92,7 @@ class Profile : AppCompatActivity() {
 
     private fun showAvatarSelectionDialog() {
         // List of available avatar animals (these should match your drawable resource names)
-        val avatarOptions = listOf("panda", "duck", "bear", "pig", "frog", "elephant", "monkey", "bober", "koala", "tiger")
+        val avatarOptions = listOf("panda", "duck", "bear", "pig", "frog","sheep", "monkey", "bober", "koala", "tiger", "elephant","lion" )
 
         // Create a grid layout for the avatars
         val builder = AlertDialog.Builder(this)
